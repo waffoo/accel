18833898b002b2e10b01486e7007d0e923303263
waffoo <mukuwaffle@gmail.com>
2020-06-03 02:15:55 +0900

    done is included by replay_buffer

--- accel/agents/dqn.py
+++ accel/agents/dqn.py
@@ -40,17 +40,15 @@ class DQN:
         return action.item()
 
     def update(self, obs, action, next_obs, reward, done):
-        if done:
-            next_obs = None
-
-        self.replay_buffer.push(obs, action, next_obs, np.float32(reward))
+        self.replay_buffer.push(obs, action, next_obs,
+                                np.float32(reward), done)
 
         self.total_steps += 1
         if self.total_steps % self.update_interval == 0:
             self.train()
 
-    def non_final_next_state_value(self, non_final_next_states):
-        return self.target_q_func(non_final_next_states).max(1)[0].detach()
+    def next_state_value(self, next_states):
+        return self.target_q_func(next_states).max(1)[0].detach()
 
     def train(self):
         if len(self.replay_buffer) < self.batch_size:
@@ -59,28 +57,22 @@ class DQN:
         transitions = self.replay_buffer.sample(self.batch_size)
         batch = Transition(*zip(*transitions))
 
-        non_final_mask = torch.tensor(tuple(map(
-            lambda s: s is not None, batch.next_state)), device=self.device)
-        next_states = np.stack([s for s in batch.next_state if s is not None])
-        non_final_next_states = torch.tensor(
-            next_states, device=self.device, dtype=torch.float32)
-
         state_batch = torch.tensor(
             np.stack(batch.state), device=self.device, dtype=torch.float32)
         action_batch = torch.tensor(
             batch.action, device=self.device, dtype=torch.int64).unsqueeze(1)
+        next_state_batch = torch.tensor(
+            np.stack(batch.next_state), device=self.device, dtype=torch.float32)
         reward_batch = torch.tensor(
             batch.reward, device=self.device, dtype=torch.float32)
+        done_batch = torch.tensor(
+            batch.done, device=self.device, dtype=torch.float32)
 
         state_action_values = self.q_func(state_batch).gather(1, action_batch)
 
-        next_state_values = torch.zeros(self.batch_size, device=self.device)
-
-        next_state_values[non_final_mask] = self.non_final_next_state_value(
-            non_final_next_states)
-
-        expected_state_action_values = (
-            next_state_values * self.gamma) + reward_batch
+        expected_state_action_values = reward_batch + \
+            (1. - done_batch) * self.gamma * \
+            self.next_state_value(next_state_batch)
 
         if self.huber:
             loss = F.smooth_l1_loss(state_action_values,
@@ -101,8 +93,8 @@ class DQN:
 
 
 class DoubleDQN(DQN):
-    def non_final_next_state_value(self, non_final_next_states):
-        next_action_batch = self.q_func(non_final_next_states).max(1)[
+    def next_state_value(self, next_states):
+        next_action_batch = self.q_func(next_states).max(1)[
             1].unsqueeze(1)
         return self.target_q_func(
-            non_final_next_states).gather(1, next_action_batch).squeeze().detach()
+            next_states).gather(1, next_action_batch).squeeze().detach()
