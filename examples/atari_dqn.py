d4e0d80c76264d03bd6d155be29ed61f975d8d25
waffoo <mukuwaffle@gmail.com>
2020-09-20 15:43:16 +0900

    rename atari example and add "with"

--- examples/atari_dqn.py
+++ examples/atari_dqn.py
@@ -7,6 +7,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 import hydra
 import mlflow
+import os
 
 from accel.utils.atari_wrappers import make_atari, make_atari_ram
 from accel.explorers import epsilon_greedy
@@ -60,180 +61,181 @@ class RamNet(nn.Module):
         return F.relu(self.fc4(x))
 
 
-@hydra.main(config_name='config/atari_ddqn_config.yaml')
+@hydra.main(config_name='config/atari_dqn_config.yaml')
 def main(cfg):
     set_seed(cfg.seed)
 
     path = hydra.utils.get_original_cwd()
-    mlflow.set_tracking_uri(path+'/mlruns')
-    mlflow.set_experiment('testexp4')
+    path = os.path.join(path, 'mlruns')
+    mlflow.set_tracking_uri(path)
+    mlflow.set_experiment('atari_dqn')
 
-    mlflow.start_run()
-    mlflow.log_param('seed', cfg.seed)
-    mlflow.log_param('gamma', cfg.gamma)
-    mlflow.log_param('replay', cfg.replay_capacity)
+    with mlflow.start_run():
+        mlflow.log_param('seed', cfg.seed)
+        mlflow.log_param('gamma', cfg.gamma)
+        mlflow.log_param('replay', cfg.replay_capacity)
+        mlflow.log_param('dueling', cfg.dueling)
 
-    if not cfg.device:
-        cfg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if not cfg.device:
+            cfg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    is_ram = '-ram' in cfg.env
-    if is_ram:
-        env = make_atari_ram(cfg.env)
-        eval_env = make_atari_ram(cfg.env, clip_rewards=False)
-    else:
-        env = make_atari(cfg.env)
-        eval_env = make_atari(cfg.env, clip_rewards=False)
+        is_ram = '-ram' in cfg.env
+        if is_ram:
+            env = make_atari_ram(cfg.env)
+            eval_env = make_atari_ram(cfg.env, clip_rewards=False)
+        else:
+            env = make_atari(cfg.env)
+            eval_env = make_atari(cfg.env, clip_rewards=False)
 
-    env.seed(cfg.seed)
-    eval_env.seed(cfg.seed)
+        env.seed(cfg.seed)
+        eval_env.seed(cfg.seed)
 
-    dim_state = env.observation_space.shape[0]
-    dim_action = env.action_space.n
+        dim_state = env.observation_space.shape[0]
+        dim_action = env.action_space.n
 
-    if is_ram:
-        q_func = RamNet(dim_state, dim_action)
-    else:
-        q_func = Net(dim_state, dim_action, dueling=cfg.dueling)
+        if is_ram:
+            q_func = RamNet(dim_state, dim_action)
+        else:
+            q_func = Net(dim_state, dim_action, dueling=cfg.dueling)
 
-    if cfg.load:
-        q_func.load_state_dict(torch.load(cfg.load, map_location=cfg.device))
+        if cfg.load:
+            q_func.load_state_dict(torch.load(cfg.load, map_location=cfg.device))
 
-    optimizer = optim.RMSprop(
-        q_func.parameters(), lr=0.00025, alpha=0.95, eps=1e-2)
-    memory = ReplayBuffer(capacity=cfg.replay_capacity)
+        optimizer = optim.RMSprop(
+            q_func.parameters(), lr=0.00025, alpha=0.95, eps=1e-2)
+        memory = ReplayBuffer(capacity=cfg.replay_capacity)
 
-    score_steps = []
-    scores = []
+        score_steps = []
+        scores = []
 
-    explorer = epsilon_greedy.LinearDecayEpsilonGreedy(
-        start_eps=1.0, end_eps=0.1, decay_steps=1e6)
+        explorer = epsilon_greedy.LinearDecayEpsilonGreedy(
+            start_eps=1.0, end_eps=0.1, decay_steps=1e6)
 
-    agent = dqn.DoubleDQN(q_func, optimizer, memory, cfg.gamma,
-                          explorer, cfg.device, batch_size=32, target_update_interval=10000)
+        agent = dqn.DoubleDQN(q_func, optimizer, memory, cfg.gamma,
+                              explorer, cfg.device, batch_size=32, target_update_interval=10000)
 
-    if cfg.demo:
-        for x in range(10):
-            total_reward = 0
+        if cfg.demo:
+            for x in range(10):
+                total_reward = 0
 
-            while True:
-                obs = eval_env.reset()
-                eval_env.render()
-                done = False
-
-                while not done:
-                    action = agent.act(obs, greedy=True)
-                    obs, reward, done, _ = eval_env.step(action)
+                while True:
+                    obs = eval_env.reset()
                     eval_env.render()
+                    done = False
 
-                    # print(reward)
-                    total_reward += reward
+                    while not done:
+                        action = agent.act(obs, greedy=True)
+                        obs, reward, done, _ = eval_env.step(action)
+                        eval_env.render()
 
-                if eval_env.was_real_done:
-                    break
+                        # print(reward)
+                        total_reward += reward
 
-            print('Episode:', x, 'Score:', total_reward)
+                    if eval_env.was_real_done:
+                        break
 
-        exit(0)
+                print('Episode:', x, 'Score:', total_reward)
 
-    next_eval_cnt = 1
-    episode_cnt = 0
+            exit(0)
 
-    train_start_time = time.time()
+        next_eval_cnt = 1
+        episode_cnt = 0
 
-    log_file_name = 'scores.txt'
-    best_score = -1e10
+        train_start_time = time.time()
 
-    while agent.total_steps < cfg.steps:
-        episode_cnt += 1
+        log_file_name = 'scores.txt'
+        best_score = -1e10
 
-        obs = env.reset()
-        done = False
-        total_reward = 0
-        step = 0
+        while agent.total_steps < cfg.steps:
+            episode_cnt += 1
 
-        while not done:
-            action = agent.act(obs)
-            next_obs, reward, done, _ = env.step(action)
-            total_reward += reward
-            step += 1
-
-            next_valid = 1 if step == env.spec.max_episode_steps else float(
-                not done)
-            agent.update(obs, action, next_obs, reward, next_valid)
-
-            obs = next_obs
-
-        if agent.total_steps > next_eval_cnt * cfg.eval_interval:
+            obs = env.reset()
+            done = False
             total_reward = 0
+            step = 0
 
-            while True:
-                obs = eval_env.reset()
-                done = False
+            while not done:
+                action = agent.act(obs)
+                next_obs, reward, done, _ = env.step(action)
+                total_reward += reward
+                step += 1
 
-                while not done:
-                    action = agent.act(obs, greedy=True)
-                    obs, reward, done, _ = eval_env.step(action)
+                next_valid = 1 if step == env.spec.max_episode_steps else float(
+                    not done)
+                agent.update(obs, action, next_obs, reward, next_valid)
 
-                    total_reward += reward
+                obs = next_obs
 
-                if eval_env.was_real_done:
-                    break
+            if agent.total_steps > next_eval_cnt * cfg.eval_interval:
+                total_reward = 0
 
-            next_eval_cnt += 1
+                while True:
+                    obs = eval_env.reset()
+                    done = False
 
-            score_steps.append(agent.total_steps)
-            scores.append(total_reward)
+                    while not done:
+                        action = agent.act(obs, greedy=True)
+                        obs, reward, done, _ = eval_env.step(action)
 
-            if total_reward > best_score:
-                model_name = f'{agent.total_steps}.model'
-                torch.save(q_func.state_dict(), model_name)
-                best_score = total_reward
+                        total_reward += reward
 
-            now = time.time()
-            elapsed = now - train_start_time
+                    if eval_env.was_real_done:
+                        break
 
-            log = f'{agent.total_steps} {total_reward} {elapsed:.1f}\n'
-            print(log, end='')
-            mlflow.log_metric('reward', total_reward, step=agent.total_steps)
+                next_eval_cnt += 1
 
-            with open(log_file_name, 'a') as f:
-                f.write(log)
+                score_steps.append(agent.total_steps)
+                scores.append(total_reward)
 
-    # final evaluation
-    total_reward = 0
+                if total_reward > best_score:
+                    model_name = f'{agent.total_steps}.model'
+                    torch.save(q_func.state_dict(), model_name)
+                    best_score = total_reward
 
-    while True:
-        obs = eval_env.reset()
-        done = False
+                now = time.time()
+                elapsed = now - train_start_time
 
-        while not done:
-            action = agent.act(obs, greedy=True)
-            obs, reward, done, _ = eval_env.step(action)
+                log = f'{agent.total_steps} {total_reward} {elapsed:.1f}\n'
+                print(log, end='')
+                mlflow.log_metric('reward', total_reward, step=agent.total_steps)
 
-            total_reward += reward
+                with open(log_file_name, 'a') as f:
+                    f.write(log)
 
-        if eval_env.was_real_done:
-            break
+        # final evaluation
+        total_reward = 0
 
-    score_steps.append(agent.total_steps)
-    scores.append(total_reward)
+        while True:
+            obs = eval_env.reset()
+            done = False
 
-    model_name = f'final.model'
-    torch.save(q_func.state_dict(), model_name)
+            while not done:
+                action = agent.act(obs, greedy=True)
+                obs, reward, done, _ = eval_env.step(action)
 
-    now = time.time()
-    elapsed = now - train_start_time
+                total_reward += reward
 
-    log = f'{agent.total_steps} {total_reward} {elapsed:.1f}\n'
-    print(log, end='')
-    mlflow.log_metric('reward', total_reward, step=agent.total_steps)
+            if eval_env.was_real_done:
+                break
 
-    with open(log_file_name, 'a') as f:
-        f.write(log)
+        score_steps.append(agent.total_steps)
+        scores.append(total_reward)
 
-    print('Complete')
-    env.close()
-    mlflow.end_run()
+        model_name = f'final.model'
+        torch.save(q_func.state_dict(), model_name)
+
+        now = time.time()
+        elapsed = now - train_start_time
+
+        log = f'{agent.total_steps} {total_reward} {elapsed:.1f}\n'
+        print(log, end='')
+        mlflow.log_metric('reward', total_reward, step=agent.total_steps)
+
+        with open(log_file_name, 'a') as f:
+            f.write(log)
+
+        print('Complete')
+        env.close()
 
 
 if __name__ == '__main__':
