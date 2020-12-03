7d855f95b2b028673d3aa84125b6beae03d0e817
waffoo <mukuwaffle@gmail.com>
2020-12-03 20:49:16 +0900

    add atari_ppo.py

--- examples/atari_ppo.py
+++ examples/atari_ppo.py
@@ -49,67 +49,24 @@ class Net(nn.Module):
         return v + adv - adv.mean(dim=1, keepdim=True)
 
 
-@hydra.main(config_name='config/atari_dqn_config.yaml')
+@hydra.main(config_name='config/atari_ppo_config.yaml')
 def main(cfg):
     set_seed(cfg.seed)
 
     cwd = hydra.utils.get_original_cwd()
     mlflow.set_tracking_uri(os.path.join(cwd, 'mlruns'))
-    mlflow.set_experiment('atari_dqn')
+    mlflow.set_experiment('atari_ppo')
 
     with mlflow.start_run(run_name=cfg.name):
         mlflow.log_param('seed', cfg.seed)
         mlflow.log_param('gamma', cfg.gamma)
-        mlflow.log_param('replay', cfg.replay_capacity)
-        mlflow.log_param('dueling', cfg.dueling)
-        mlflow.log_param('prioritized', cfg.prioritized)
         mlflow.log_param('color', cfg.color)
         mlflow.log_param('high', cfg.high_reso)
         mlflow.log_param('no_stack', cfg.no_stack)
-        mlflow.log_param('nstep', cfg.nstep)
-        mlflow.log_param('adam', cfg.adam)
-        mlflow.log_param('end_eps', cfg.end_eps)
         mlflow.set_tag('env', cfg.env)
 
         if not cfg.device:
             cfg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-        if cfg.high_reso:
-            env = make_atari(cfg.env, color=cfg.color,
-                             image_size=128, frame_stack=not cfg.no_stack)
-            eval_env = make_atari(
-                cfg.env, clip_rewards=False, color=cfg.color, image_size=128, frame_stack=not cfg.no_stack)
-        else:
-            env = make_atari(cfg.env, color=cfg.color,
-                             frame_stack=not cfg.no_stack)
-            eval_env = make_atari(
-                cfg.env, clip_rewards=False, color=cfg.color, frame_stack=not cfg.no_stack)
-
-        env.seed(cfg.seed)
-        eval_env.seed(cfg.seed)
-
-        dim_state = env.observation_space.shape[0]
-        dim_action = env.action_space.n
-
-        q_func = Net(dim_state, dim_action,
-                     dueling=cfg.dueling, high_reso=cfg.high_reso)
-
-        if cfg.load:
-            q_func.load_state_dict(torch.load(os.path.join(
-                cwd, cfg.load), map_location=cfg.device))
-
-        if cfg.adam:
-            # same as Rainbow
-            optimizer = optim.Adam(q_func.parameters(), lr=0.0000625, eps=1.5e-4)
-        else:
-            optimizer = optim.RMSprop(
-                q_func.parameters(), lr=0.00025, alpha=0.95, eps=1e-2)
-
-        explorer = epsilon_greedy.LinearDecayEpsilonGreedy(
-            start_eps=1.0, end_eps=cfg.end_eps, decay_steps=1e6)
-
-
-        #=========== PPO =======================
 
         wrapper = callable_atari_wrapper(color=cfg.color, frame_stack=not cfg.no_stack)
         eval_wrapper = callable_atari_wrapper(color=cfg.color, frame_stack=not cfg.no_stack, clip_rewards=False)
