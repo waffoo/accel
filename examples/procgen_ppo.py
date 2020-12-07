4f525b0031c39cb687653c1c10607a8e8a636f76
waffoo <mukuwaffle@gmail.com>
2020-12-07 17:45:14 +0900

    add mlflow settings in procgen_ppo.py

--- examples/procgen_ppo.py
+++ examples/procgen_ppo.py
@@ -5,6 +5,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from accel.agents.ppo import PPO
 import hydra
+import mlflow
+from accel.utils.utils import set_seed
+import os
 
 def init(module, weight_init, bias_init, gain=1.):
     weight_init(module.weight.data, gain=gain)
@@ -74,34 +77,54 @@ class CriticNet(nn.Module):
 
 @hydra.main(config_name='config/procgen_ppo_config.yaml')
 def main(cfg):
-    wrapper = callable_procgen_wrapper(frame_stack=not cfg.no_stack, color=cfg.color)
-    envs = gym.vector.make(cfg.env, cfg.parallel, start_level=0, num_levels=cfg.num_levels,
-                           distribution_mode='easy', wrappers=wrapper)
+    set_seed(cfg.seed)
 
-    eval_env = gym.make(cfg.env, start_level=0, num_levels=cfg.num_levels, distribution_mode='easy')
-    eval_env = wrapper(eval_env)
+    cwd = hydra.utils.get_original_cwd()
+    if cfg.load:
+        cfg.load = os.path.join(cwd, cfg.load)
+    mlflow.set_tracking_uri(os.path.join(cwd, 'mlruns'))
+    mlflow.set_experiment('procgen')
 
-    if not cfg.device:
-        cfg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    with mlflow.start_run(run_name=cfg.name):
+        mlflow.log_param('seed', cfg.seed)
+        mlflow.log_param('gamma', cfg.gamma)
+        mlflow.log_param('parallel', cfg.parallel)
+        mlflow.log_param('color', cfg.color)
+        mlflow.log_param('no_stack', cfg.no_stack)
+        mlflow.log_param('batch_size', cfg.batch_size)
+        mlflow.log_param('horizon', cfg.horizon)
+        mlflow.log_param('num_levels', cfg.num_levels)
+        mlflow.set_tag('env', cfg.env)
 
-    '''
-    obs = envs.reset()
-    obs, reward, done, info = envs.step(envs.action_space.sample())
-    for i in range(8):
-        print(info[i]['level_seed'])
-    '''
+        wrapper = callable_procgen_wrapper(frame_stack=not cfg.no_stack, color=cfg.color)
+        envs = gym.vector.make(cfg.env, cfg.parallel, start_level=0, num_levels=cfg.num_levels,
+                               distribution_mode='easy', wrappers=wrapper)
 
-    dim_state = envs.observation_space.shape[1]
-    dim_action = envs.action_space[0].n
+        eval_env = gym.make(cfg.env, start_level=0, num_levels=cfg.num_levels, distribution_mode='easy')
+        eval_env = wrapper(eval_env)
 
-    actor = ActorNet(dim_state, dim_action)
-    critic = CriticNet(dim_state)
+        if not cfg.device:
+            cfg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    agent = PPO(envs, eval_env, steps=cfg.steps, actor=actor, critic=critic, lmd=0.9,
-                gamma=cfg.gamma, device=cfg.device,
-                batch_size=cfg.batch_size, load=cfg.load, eval_interval=cfg.eval_interval, clip_eps=0.1,
-                mlflow=True, value_loss_coef=cfg.value_loss_coef, value_clipping=True, epoch_per_eval=cfg.epoch_per_eval)
-    agent.run()
+        '''
+        obs = envs.reset()
+        obs, reward, done, info = envs.step(envs.action_space.sample())
+        for i in range(8):
+            print(info[i]['level_seed'])
+        '''
+
+        dim_state = envs.observation_space.shape[1]
+        dim_action = envs.action_space[0].n
+
+        actor = ActorNet(dim_state, dim_action)
+        critic = CriticNet(dim_state)
+
+        agent = PPO(envs, eval_env, steps=cfg.steps, actor=actor, critic=critic, lmd=0.9,
+                    gamma=cfg.gamma, device=cfg.device,
+                    batch_size=cfg.batch_size, load=cfg.load, eval_interval=cfg.eval_interval, clip_eps=0.1,
+                    mlflow=True, value_loss_coef=cfg.value_loss_coef, value_clipping=True,
+                    epoch_per_eval=cfg.epoch_per_eval, horizon=cfg.horizon)
+        agent.run()
 
 
 if __name__ == '__main__':
