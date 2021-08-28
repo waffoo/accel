3318ef19c10f610b3cdb92e56664f7226b155750
waffoo <mukuwaffle@gmail.com>
2021-08-28 18:57:02 +0900

    Feature/replace mlflow (#22)
    * replace mlflow with comet in bullet_sac
    
    * replace mlflow in atari_dqn
    
    * replace mlflow in atari_cql
    
    * replace mlflow in atari_ppo and remove mlflow from dependency

--- examples/atari_cql.py
+++ examples/atari_cql.py
@@ -1,9 +1,10 @@
 import os
+from logging import DEBUG, getLogger
 from time import time
 
+from comet_ml import Experiment  # isort: split
 import gym
 import hydra
-import mlflow
 import numpy as np
 # from gym.utils.play import play import random
 import torch
@@ -18,6 +19,9 @@ from accel.replay_buffers.prioritized_replay_buffer import \
 from accel.replay_buffers.replay_buffer import ReplayBuffer, Transition
 from accel.utils.atari_wrappers import make_atari
 from accel.utils.utils import set_seed
+
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
 
 
 class Net(nn.Module):
@@ -57,94 +61,114 @@ def main(cfg):
     set_seed(cfg.seed)
 
     cwd = hydra.utils.get_original_cwd()
-    mlflow.set_tracking_uri(os.path.join(cwd, 'mlruns'))
-    mlflow.set_experiment('atari_dqn')
 
-    with mlflow.start_run(run_name=cfg.name):
-        mlflow.log_param('seed', cfg.seed)
-        mlflow.log_param('gamma', cfg.gamma)
-        mlflow.log_param('replay', cfg.replay_capacity)
-        mlflow.log_param('dueling', cfg.dueling)
-        mlflow.log_param('prioritized', cfg.prioritized)
-        mlflow.log_param('color', cfg.color)
-        mlflow.log_param('high', cfg.high_reso)
-        mlflow.log_param('no_stack', cfg.no_stack)
-        mlflow.log_param('nstep', cfg.nstep)
-        mlflow.log_param('adam', cfg.adam)
-        mlflow.log_param('end_eps', cfg.end_eps)
-        mlflow.set_tag('env', cfg.env)
+    if cfg.comet:
+        comet_username = os.environ['COMET_USERNAME']
+        comet_api_token = os.environ['COMET_API_TOKEN']
+        logger.debug(f'Comet username: {comet_username}')
 
-        if not cfg.device:
-            cfg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if not cfg.device:
+        cfg.device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-        if cfg.high_reso:
-            env = make_atari(cfg.env, color=cfg.color,
-                             image_size=128, frame_stack=not cfg.no_stack)
-            eval_env = make_atari(
-                cfg.env, clip_rewards=False, color=cfg.color, image_size=128, frame_stack=not cfg.no_stack)
-        else:
-            env = make_atari(cfg.env, color=cfg.color,
-                             frame_stack=not cfg.no_stack)
-            eval_env = make_atari(
-                cfg.env, clip_rewards=False, color=cfg.color, frame_stack=not cfg.no_stack)
+    if cfg.high_reso:
+        env = make_atari(cfg.env, color=cfg.color,
+                         image_size=128, frame_stack=not cfg.no_stack)
+        eval_env = make_atari(
+            cfg.env, clip_rewards=False, color=cfg.color, image_size=128, frame_stack=not cfg.no_stack)
+    else:
+        env = make_atari(cfg.env, color=cfg.color,
+                         frame_stack=not cfg.no_stack)
+        eval_env = make_atari(
+            cfg.env, clip_rewards=False, color=cfg.color, frame_stack=not cfg.no_stack)
 
-        env.seed(cfg.seed)
-        eval_env.seed(cfg.seed)
+    env.seed(cfg.seed)
+    eval_env.seed(cfg.seed)
 
-        dim_state = env.observation_space.shape[0]
-        dim_action = env.action_space.n
+    dim_state = env.observation_space.shape[0]
+    dim_action = env.action_space.n
 
-        q_func = Net(dim_state, dim_action,
-                     dueling=cfg.dueling, high_reso=cfg.high_reso)
+    q_func = Net(dim_state, dim_action,
+                 dueling=cfg.dueling, high_reso=cfg.high_reso)
 
-        if cfg.load:
-            q_func.load_state_dict(torch.load(os.path.join(
-                cwd, cfg.load), map_location=cfg.device))
+    if cfg.load:
+        q_func.load_state_dict(torch.load(os.path.join(
+            cwd, cfg.load), map_location=cfg.device))
 
-        if cfg.adam:
-            # same as Rainbow
-            optimizer = optim.Adam(q_func.parameters(),
-                                   lr=0.0000625, eps=1.5e-4)
-        else:
-            optimizer = optim.RMSprop(
-                q_func.parameters(), lr=0.00025, alpha=0.95, eps=1e-2)
+    if cfg.adam:
+        # same as Rainbow
+        optimizer = optim.Adam(q_func.parameters(),
+                               lr=0.0000625, eps=1.5e-4)
+    else:
+        optimizer = optim.RMSprop(
+            q_func.parameters(), lr=0.00025, alpha=0.95, eps=1e-2)
 
-        if cfg.prioritized:
-            memory = PrioritizedReplayBuffer(
-                capacity=cfg.replay_capacity, beta_steps=cfg.steps - cfg.replay_start_step, nstep=cfg.nstep)
-        else:
-            memory = ReplayBuffer(capacity=cfg.replay_capacity, nstep=cfg.nstep, record=True, record_size=cfg.record_size,
-                                  record_outdir=os.path.join(cwd, cfg.record_outdir, cfg.name))
+    if cfg.prioritized:
+        memory = PrioritizedReplayBuffer(
+            capacity=cfg.replay_capacity, beta_steps=cfg.steps - cfg.replay_start_step, nstep=cfg.nstep)
+    else:
+        memory = ReplayBuffer(capacity=cfg.replay_capacity, nstep=cfg.nstep, record=True, record_size=cfg.record_size,
+                              record_outdir=os.path.join(cwd, cfg.record_outdir, cfg.name))
 
-        explorer = epsilon_greedy.LinearDecayEpsilonGreedy(
-            start_eps=1.0, end_eps=cfg.end_eps, decay_steps=1e6)
+    explorer = epsilon_greedy.LinearDecayEpsilonGreedy(
+        start_eps=1.0, end_eps=cfg.end_eps, decay_steps=1e6)
 
-        outdir = os.path.join(cwd, 'results', cfg.name)
-        agent = dqn_cql.DQN_CQL(eval_env=eval_env, outdir=outdir, cql_weight=4.,
-                                q_func=q_func, optimizer=optimizer,
-                                replay_buffer=memory, gamma=cfg.gamma,
-                                explorer=explorer, device=cfg.device, batch_size=32,
-                                target_update_interval=10000,
-                                replay_start_step=cfg.replay_start_step)
+    if cfg.comet:
+        comet_exp = Experiment(project_name='accel',
+                               api_key=comet_api_token,
+                               workspace=comet_username)
+        comet_exp.add_tag('atari_cql')
+        comet_exp.add_tag(cfg.env)
+        comet_exp.set_name(cfg.name)
 
-        data_path = os.path.join(cwd, 'dataset', 'pong-1s', '9.npz')
-        print(f'open {data_path}...')
-        dataset = np.load(data_path)
-        print(dataset.files)
+        comet_params = {
+            'seed': cfg.seed,
+            'gamma': cfg.gamma,
+            'replay': cfg.replay_capacity,
+            'dueling': cfg.dueling,
+            'prioritized': cfg.prioritized,
+            'color': cfg.color,
+            'high': cfg.high_reso,
+            'no_stack': cfg.no_stack,
+            'nstep': cfg.nstep,
+            'adam': cfg.adam,
+            'end_eps': cfg.end_eps,
+            'env': cfg.env,
+        }
 
-        agent.set_dataset(dataset)
+        comet_exp.log_parameters(comet_params)
+    else:
+        comet_exp = None
 
-        agent.fit(cfg.steps, cfg.eval_interval)
+    outdir = os.path.join(cwd, 'results', cfg.name)
+    agent = dqn_cql.DQN_CQL(eval_env=eval_env, outdir=outdir, cql_weight=4.,
+                            q_func=q_func, optimizer=optimizer,
+                            replay_buffer=memory, gamma=cfg.gamma,
+                            explorer=explorer, device=cfg.device, batch_size=32,
+                            target_update_interval=10000,
+                            replay_start_step=cfg.replay_start_step,
+                            comet_exp=comet_exp
+                            )
 
-        agent.eval(render=True)
+    data_path = os.path.join(cwd, 'dataset', 'pong-1s', '9.npz')
+    print(f'open {data_path}...')
+    dataset = np.load(data_path)
+    print(dataset.files)
 
-        now = time()
-        elapsed = now - agent.train_start_time
+    agent.set_dataset(dataset)
 
-        duration = np.round(elapsed / 60 / 60, 2)
-        mlflow.log_metric('duration', duration)
-        print('Complete')
-        env.close()
+    agent.fit(cfg.steps, cfg.eval_interval)
+
+    agent.eval(render=True)
+
+    now = time()
+    elapsed = now - agent.train_start_time
+
+    duration = np.round(elapsed / 60 / 60, 2)
+    if cfg.comet:
+        comet_exp.log_metric('duration', duration)
+
+    print('Complete')
+    env.close()
 
 
 if __name__ == '__main__':
