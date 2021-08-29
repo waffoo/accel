f67d27854229a087525763c99d73a542e3896a04
waffoo <mukuwaffle@gmail.com>
2021-08-29 16:05:27 +0900

    add decimation in save_as_video in util

--- accel/utils/utils.py
+++ accel/utils/utils.py
@@ -15,7 +15,15 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
 
 
-def save_as_video(name, frames, fps=60):
+def save_as_video(name, frames, fps=60, decimate=1):
     Path(name).parent.mkdir(exist_ok=True, parents=True)
+    if decimate > 1:
+        new_frames = []
+        for i, f in enumerate(frames):
+            if i % decimate == 0:
+                new_frames.append(f)
+        frames = new_frames
+        fps /= decimate
+
     imageio.mimsave(name, frames, fps=fps)
     pygifsicle.optimize(name)
